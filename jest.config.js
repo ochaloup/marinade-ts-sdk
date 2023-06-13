8a885738229514fbe80989fce60a3be3be44e3b9
Ondra Chaloupka <chalda@marinade.finance>
2023-06-13 22:33:57 +0200

    configuration of tests fixing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+
+// we can define here only configuration that belongs to both test types,
+// to unit tests (src/**/*.spec.ts) and integration tests (test/**/*.spec.ts)
+
+module.exports = {    
     preset: 'ts-jest',
-    testEnvironment: 'node',
-    testTimeout: 90000,
-    roots: ['<rootDir>/test'],
-    globalSetup: '<rootDir>/test/setup/globalSetup.ts',
     setupFilesAfterEnv: [
       '<rootDir>/test/setup/equalityTesters.ts',
     ],
